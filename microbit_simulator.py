34d668ab75f8d69bc037e80ddec6c39a65eb265d
chris <chall168421@gmail.com>
2023-01-12 16:07:17 +0700

    first commit - early plans

---
+++ microbit_simulator.py
@@ -0,0 +1,47 @@
+def coord_to_int(x, y):
+
+
+
+
+
+
+    
+    100 10 1
+        1010
+
+    110
+
+    
+
+
+class MicroBitSim:
+    def __init__(self):
+        self.leds = 0 # An integer based around 25 b values
+        
+    def set_pixel(self, x, y, b):
+        
+        self.display()
+
+    def show(self, img):
+        self.display()
+
+
+    def scroll(self):
+
+        self.display()
+
+    def display(self):
+        # brightness map for each pixel
+
+        # decide what  colour to display
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    
+        
